a0e4978e2accf7edf354bd210ecfc7f5f3f4f2dd
Diego Arthur <digo.arthur@hotmail.com>
2023-05-08 20:33:02 +0100

    feat: initial development of the toPutCardsOnTheTable function.

---
+++ script.js
@@ -0,0 +1,65 @@
+const deckCard = [
+    "club_2.svg",
+    "club_5.svg",
+    "club_A.svg",
+    "club_J.svg",
+    "club_K.svg",
+    "diamond_3.svg",
+    "diamond_6.svg",
+    "diamond_7.svg",
+    "diamond_A.svg",
+    "diamond_J.svg",
+    "diamond_K.svg",
+    "diamond_Q.svg",
+    "heart_4.svg",
+    "heart_6.svg",
+    "heart_10.svg",
+    "heart_A.svg",
+    "heart_Q.svg",
+    "spade_2.svg",
+    "spade_5.svg",
+    "spade_8.svg",
+    "spade_A.svg",
+];
+
+
+let cardHTML = "";
+const column01 = document.querySelector("#column01");
+const column02 = document.querySelector("#column02");
+const column03 = document.querySelector("#column03");
+var i = 0;
+var positionTop = 0;                  //  set your counter to 1
+var positionTop2 = 0;
+var positionTop3 = 0;
+
+function myLoop(array) {         //  create a loop function
+    setTimeout(function () {   //  call a 3s setTimeout when the loop is called
+        if (i < 7) {
+            column01.innerHTML += `<img style="position: absolute; top:${positionTop += 50}px" src="./cards/${array[i]}"/>`
+        }
+        if (i >= 7 && i < 14) {
+            column02.innerHTML += `<img style="position: absolute; top:${positionTop2 += 50}px" src="./cards/${array[i]}"/>`
+        }
+        if (i >= 14 && i < 21) {
+            column03.innerHTML += `<img style="position: absolute; top:${positionTop3 += 50}px" src="./cards/${array[i]}"/>`
+        }
+
+        i++;                    //  increment the counter
+        if (i < deckCard.length) {           //  if the counter < 10, call the loop function
+            myLoop(array);             //  ..  again which will trigger another 
+        }                       //  ..  setTimeout()
+    }, 200)
+}
+
+myLoop(deckCard);
+
+
+
+
+
+
+
+
+
+
+
