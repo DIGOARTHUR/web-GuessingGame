d8d4049770a242443382e42bb8cbd455ab96138b
digoarthur <digo.arthur@hotmail.com>
2023-06-23 19:57:28 +0100

    refactor: change name function and variables

--- script.js
+++ script.js
@@ -23,29 +23,35 @@ let deckCard = [
 ];
 
 
-var column01 = document.querySelector("#column01");
-var column02 = document.querySelector("#column02");
-var column03 = document.querySelector("#column03");
+var leftColumn = document.querySelector("#leftColumn");
+var middleColumn = document.querySelector("#middleColumn");
+var rightColumn = document.querySelector("#rightColumn");
 var countCards = 0;
 var positionTop = 0;
 var toggleColumn = 1
 
-function toPutCardsOnTheTable(array) {
+
+
+
+toPutCardsOnTheTable(deckCard);
+
+
+function toPutCardsOnTheTable(deckCard) {
 
     setTimeout(() => {
         if (countCards < 21) {
             switch (toggleColumn) {
                 case 1:
                     console.log(toggleColumn)
-                    column01.innerHTML += `<img class='imgCard' style="position: absolute; top:${positionTop}px" src="./cards/${array[countCards]}"/>`
+                    leftColumn.innerHTML += `<img class='imgCard' style="position: absolute; top:${positionTop}px" src="./cards/${deckCard[countCards]}"/>`
                     toggleColumn += 1;
                     break;
                 case 2:
-                    column02.innerHTML += `<img class='imgCard2' style="position: absolute; top:${positionTop}px" src="./cards/${array[countCards]}"/>`
+                    middleColumn.innerHTML += `<img class='imgCard2' style="position: absolute; top:${positionTop}px" src="./cards/${deckCard[countCards]}"/>`
                     toggleColumn += 1;
                     break;
                 case 3:
-                    column03.innerHTML += `<img class='imgCard3' style="position: absolute; top:${positionTop}px" src="./cards/${array[countCards]}"/>`
+                    rightColumn.innerHTML += `<img class='imgCard3' style="position: absolute; top:${positionTop}px" src="./cards/${deckCard[countCards]}"/>`
                     toggleColumn = 1
                     positionTop += 50
                     break;
@@ -53,7 +59,7 @@ function toPutCardsOnTheTable(array) {
         }
         countCards++;
         if (countCards < deckCard.length) {
-            toPutCardsOnTheTable(array);
+            toPutCardsOnTheTable(deckCard);
         } else {
             countCards = 0
             positionTop = 0;
@@ -65,74 +71,39 @@ function toPutCardsOnTheTable(array) {
 
 
 
-let cont = 0
-
-function result() {
-
-    console.log(deckCard[10])
-
-}
 
 
-toPutCardsOnTheTable(deckCard);
-let newDeckCard = []
-const file1 = [20, 17, 14, 11, 8, 5, 2, 18, 15, 12, 9, 6, 3, 0, 19, 16, 13, 10, 7, 4, 1]
+function selectColumn(column) {
+    let newDeckCard = []
 
-const file2 = [20, 17, 14, 11, 8, 5, 2, 19, 16, 13, 10, 7, 4, 1, 18, 15, 12, 9, 6, 3, 0]
-
-const file3 = [18, 15, 12, 9, 6, 3, 0, 20, 17, 14, 11, 8, 5, 2, 19, 16, 13, 10, 7, 4, 1,]
-
-
-var contador = 0;
-function myFunction(op) {
-
-    if (contador == 3) {
-        console.log(deckCard[10])
-    }
-    console.log(op)
-    switch (op) {
+    const shuffleForSelect_leftColumn = [20, 17, 14, 11, 8, 5, 2, 18, 15, 12, 9, 6, 3, 0, 19, 16, 13, 10, 7, 4, 1]
+    const shuffleForSelect_middletColumn = [20, 17, 14, 11, 8, 5, 2, 19, 16, 13, 10, 7, 4, 1, 18, 15, 12, 9, 6, 3, 0]
+    const shuffleForSelect_rightColumn = [18, 15, 12, 9, 6, 3, 0, 20, 17, 14, 11, 8, 5, 2, 19, 16, 13, 10, 7, 4, 1,]
+    switch (column) {
         case 1: for (let index in deckCard) {
 
-            newDeckCard.push(deckCard[file1[index]])
-
-
+            newDeckCard.push(deckCard[shuffleForSelect_leftColumn[index]])
         }
-            deckCard = newDeckCard
-            newDeckCard = []
-
-            clearTable()
-            result()
-            contador++;
-            break
 
         case 2: for (let index in deckCard) {
 
-            newDeckCard.push(deckCard[file2[index]])
+            newDeckCard.push(deckCard[shuffleForSelect_middletColumn[index]])
 
         }
-            deckCard = newDeckCard
-            newDeckCard = []
-            clearTable()
-            result()
-            contador++;
-            break
+
         case 3: for (let index in deckCard) {
 
-            newDeckCard.push(deckCard[file3[index]])
-
+            newDeckCard.push(deckCard[shuffleForSelect_rightColumn[index]])
         }
-
-            deckCard = newDeckCard
-            newDeckCard = []
-
-            clearTable()
-            contador++;
-            result();
-            break
     }
 
+    deckCard = newDeckCard
+    clearTable();
+    result();
 
 }
+
+
 
 
 function clearTable() {
@@ -140,19 +111,19 @@ function clearTable() {
     for (let i = 0; i < 7; i++) {
         var child = document.querySelector(".imgCard");
 
-        column01.removeChild(child)
+        leftColumn.removeChild(child)
 
     }
     for (let i = 0; i < 7; i++) {
         var child = document.querySelector(".imgCard2");
 
-        column02.removeChild(child)
+        middleColumn.removeChild(child)
 
     }
     for (let i = 0; i < 7; i++) {
         var child = document.querySelector(".imgCard3");
 
-        column03.removeChild(child)
+        rightColumn.removeChild(child)
 
     }
     toPutCardsOnTheTable(deckCard);
@@ -160,5 +131,9 @@ function clearTable() {
 
 
 
+function result() {
 
+    console.log(deckCard[10])
+
+}
 
